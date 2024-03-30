cc1a1fa42c7c67afc2dc33a90a24ba995677c50f
Larissa Corrêa <larimscorrea@gmail.com>
2024-03-30 17:56:02 -0300

    caminhando

--- app/src/main/java/com/example/saborcasa/LayoutEntradasActivity.kt
+++ app/src/main/java/com/example/saborcasa/LayoutEntradasActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity5 : AppCompatActivity() {
+class LayoutEntradasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_entradas)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
